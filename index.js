a2b8eb999c15dbea94bf1279226db7b3fd9b38f6
Constance Chapman <constancealicechapman224@gmail.com>
2018-09-21 17:06:53 +0100

    index and show endpoints, create endpoint wip

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const parse = require('csv-parse');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'jokes.csv',
+    header: [
+      {id: 'name', title: 'NAME'},
+      {id: 'lang', title: 'LANGUAGE'}
+    ]
+});
+let allJokes;
 
 fs.readFile('jokes.csv', (err, data) => {
   parse(data, {}, (err, jokes) => {
+    allJokes = {jokes: jokes};
+
     app.get('/', function (req, res) {
       let i = Math.floor((Math.random() * jokes.length));
       let response = {
@@ -12,8 +23,36 @@ fs.readFile('jokes.csv', (err, data) => {
       }
       res.json(response);
     });
+
+    app.get('/jokes', function (req, res) {
+      let response = {
+        jokes: allJokes
+      };
+      res.json(response);
+    });
+
+    app.post('/jokes/new', function (req, res) {
+      const records = [
+          {name: 'Bob',  lang: 'French, English'},
+          {name: 'Mary', lang: 'English'}
+        ];
+      // allJokes.jokes.push(newJoke);
+      // res.send(allJokes);
+      // const writeJokes = [];
+      // allJokes.jokes.forEach((joke) => writeJokes.push(joke[0]));
+      // console.log(writeJokes);
+      csvWriter.writeRecords(records)       // returns a promise
+        .then(() => {
+            console.log('...Done');
+        });
+    });
   });
 });
+
+
+
+
+
 
 app.listen(3050, function () {
   console.log('Example app listening on port 3050.');
