972c340d55c5b36dbb6d89802a793472edfd1c92
Juan Carlos Gallardo <juanki_12@hotmail.com>
2019-02-03 13:51:40 +0000

    First commit: CRUD endpoints, mongo database, linting
    On startup the contents of jokes.csv will be written into an in-memory mongodb database. The operations done through the exposed endpoints will modify this database.

--- index.js
+++ index.js
@@ -1,20 +1,117 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const parse = require('csv-parse');
+const express = require("express");
+const fs = require("fs");
+const debug = require("debug")("jokes:index.js");
+const mongoose = require("mongoose");
+const MongoMemoryDb = require("mongodb-memory-server").MongoMemoryServer;
 
-fs.readFile('jokes.csv', (err, data) => {
-  parse(data, {}, (err, jokes) => {
-    app.get('/', function (req, res) {
-      let i = Math.floor((Math.random() * jokes.length));
-      let response = {
-        joke: jokes[i][0]
+const app = express();
+const parse = require("csv-parse");
+
+const mongoServer = new MongoMemoryDb();
+
+let jokeSet;
+let JokeModel;
+
+fs.readFile("jokes.csv", (err, data) => {
+  parse(data, {}, (error, jokes) => {
+    jokeSet = jokes;
+    return jokes;
+  });
+});
+
+async function mongoHandler() {
+  const mongoUrl = await mongoServer.getConnectionString();
+  const mongooseOpts = {
+    autoReconnect: true,
+    reconnectTries: 5,
+    reconnectInterval: 1000
+  };
+
+  await mongoose.connect(mongoUrl, mongooseOpts);
+
+  mongoose.connection.on("error", e => {
+    debug(e);
+    mongoose.connect(mongoUrl, mongooseOpts);
+  });
+
+  mongoose.connection.once("open", () => {
+    debug(`MongoDB successfully connected to ${mongoUrl}`);
+  });
+
+  const jokeSchema = new mongoose.Schema({
+    joke: String
+  });
+
+  JokeModel = mongoose.model("Jokes", jokeSchema);
+
+  jokeSet.forEach(async function(item) {
+    debug(`Saving joke to MongoDB: ${item}`);
+    const joke = new JokeModel({ joke: item });
+    joke.save(function(err) {
+      if (err) {
+        debug(err);
       }
-      res.json(response);
+    });
+  });
+}
+
+app.get("/jokes/alljokes", function(req, res) {
+  JokeModel.find(function(err, jokes) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(jokes);
+    return jokes;
+  });
+});
+
+app.get("/jokes", function(req, res) {
+  JokeModel.find({ joke: req.query.joke }, function(err, joke) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(joke);
+  });
+});
+
+app.post("/jokes", function(req, res) {
+  try {
+    const newJoke = new JokeModel({ joke: req.query.joke });
+    newJoke.save();
+    res.status(200).send(newJoke);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.patch("/jokes", function(req, res) {
+  JokeModel.findById(req.query.id, function(err, joke) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    debug(req.query);
+    joke.joke = req.query.newJoke; // eslint-disable-line
+    joke.save(function(error, updatedJoke) {
+      if (error) {
+        res.status(500).send(error);
+      }
+      res.status(200).send(updatedJoke);
     });
   });
 });
 
-app.listen(3050, function () {
-  console.log('Example app listening on port 3050.');
+app.delete("/jokes", function(req, res) {
+  JokeModel.findById(req.query.id, function(err, joke) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    debug(req.query);
+    joke.delete();
+    res.status(200).send(joke);
+  });
+});
+
+app.listen(3050, function() {
+  debug("Starting application.");
+  mongoHandler();
 });
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Jokes database handler
+## How to
+TODO
+## Console output
+This project uses the debug library, to see all console output from the node application use:
+
+`export DEBUG=*` (bash) or `set DEBUG=*` (Windows)
+
+If you only want to see lines relevant to the project, use:
+
+`export DEBUG=jokes*` (bash) or `set DEBUG=jokes*` (Windows)
